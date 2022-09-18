fb90569a873fc962bd3fd1a58fcff0d9b80fb215
Semenov <semenov@mail.ru>
2022-09-18 15:03:03 +0400

    BACK-1 изучить темы для поднятие nest.js

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Log4jsModule } from '@nestx-log4js/core';
 import {ConfigModule} from "@nestjs/config";
+import {PostgresModule} from "nest-postgres";
+import { configModule } from 'configure.root';
 @Module({
   imports: [
     Log4jsModule.forRoot(),
-    ConfigModule.forRoot()
+    ConfigModule.forRoot(),
+    configModule,
+    PostgresModule.forRoot({
+      connectionString: process.env.URL_BD
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
