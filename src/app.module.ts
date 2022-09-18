a73f11e3f21d5066555cb84e7be78f30f1830249
Semenov <semenov@mail.ru>
2022-09-18 14:33:32 +0400

    BACK-1 изучить темы для поднятие nest.js

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Log4jsModule } from '@nestx-log4js/core';
+import {ConfigModule} from "@nestjs/config";
 @Module({
   imports: [
-    Log4jsModule.forRoot()
+    Log4jsModule.forRoot(),
+    ConfigModule.forRoot()
   ],
   controllers: [AppController],
   providers: [AppService],
