55b44ed6bf3abdf453c5ab832cfd3b858f2d5bb8
Semenov <semenov@mail.ru>
2022-09-18 21:56:22 +0400

    BACK-2 функционал проверки права

---
+++ src/api/right/RightModule.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { RightService } from './RightService';
+
+@Module({
+  controllers: [],
+  providers: [RightService],
+  exports: [RightService]
+})
+export class RightModule {}
---
+++ src/api/right/RightService.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { Client } from 'pg';
+import { InjectClient } from 'nest-postgres';
+
+@Injectable()
+export class RightService {
+  constructor(
+      @InjectClient() private readonly pg: Client,
+  ) {}
+
+  public async checkRightFun(schema: string, name: string) {
+    const result = await this.pg.query(`select * from tec.right_fun_get_find('${schema}', '${name}')`);
+    return result.rows;
+  }
+    public async checkRightUser(token:string, id: number) {
+        const result = await this.pg.query(`select * from tec.right_user_check('${token}', '${id}');`);
+        return result.rows;
+    }
+}
