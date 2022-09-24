707be515251067b08f26b9e19bf35ee3a14e6e08
Semenov <semenov@mail.ru>
2022-09-24 21:21:16 +0400

    BACK-4 api получения screen

--- src/api/screen/ScreenService.ts
+++ src/api/screen/ScreenService.ts
@@ -13,7 +13,7 @@ export class ScreenService {
     }
 
     public async screen(id: number, authorization: string) {
-       const right = await this.rightService.getRightUser(authorization);
+       const right = await this.rightService.getRightUser(authorization);      
        const result = await this.pg.query(`select * from config.screen_get_id_component(${id})`);
        return await this.checkScreen(right?.right, result.rows[0]?.screen);
     }
@@ -28,14 +28,24 @@ export class ScreenService {
             return error.rows[0];
         }
         if (screen.component) {
-            let component:any = [];
-            screen.component.map((e: any) => {
-                if (e.id_right && right.filter((r: any) => r === e.id_right).length) {
-                    component.push(e);
-                } else if (e.id_right === null) {
-                    component.push(e);
+            let component:any = {};
+            screen.component.map((e: any) => {   
+                e.params = JSON.parse(e.params);
+                e.event = JSON.parse(e.event);
+                e.schema = JSON.parse(e.schema);
+                if (e.id_right === null || e.id_right && right.filter((r: any) => r === e.id_right).length) {
+                    component[e.id] = e;
                 }
             })
+            for (const key in component) {
+                const element = component[key];
+                if (element.id_parent) {
+                    if (!component[element.id_parent].children) {
+                        component[element.id_parent].children = [];
+                    }
+                    component[element.id_parent].children.push(element.id);
+                }                
+            }             
             /** Не очень хорошо изменения значения по ссылке */
             screen.component = component;
         }
