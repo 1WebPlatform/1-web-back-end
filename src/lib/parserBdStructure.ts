c62a2450e00a2b536ea262156b4b315025f1a3d5
Semenov <semenov@mail.ru>
2022-09-27 19:47:52 +0400

    BACK-5 функционал обработки out

---
+++ src/lib/parserBdStructure.ts
@@ -0,0 +1,9 @@
+import { QueryResult } from 'pg';
+
+export function parserBdStructure(result: QueryResult) {
+  if (result.rows[0].error_) {
+    return result.rows[0];
+  }
+  return result.rows;
+}
+
