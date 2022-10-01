b526aada6dc035e1122b7c536fdb3373fbe46316
Semenov <semenov@mail.ru>
2022-10-01 20:57:00 +0400

    BACK-8 статическая обработка файлов

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Log4jsLogger } from '@nestx-log4js/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useLogger(app.get(Log4jsLogger));
   app.enableCors();
+  console.log(join(__dirname, '../../public'));  
+  app.useStaticAssets(join(__dirname, '../../public'),{
+    prefix: "/public/"
+  })
   const config = new DocumentBuilder()
     .setTitle('1-web')
     .setDescription('API back-end 1-web')
