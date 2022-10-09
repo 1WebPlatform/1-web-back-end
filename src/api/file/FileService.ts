16e8250735889ce12c2e4cdccc82b4b806589b4d
Semenov <semenov@mail.ru>
2022-10-09 13:20:15 +0400

    BACK-11 service file

--- src/api/file/FileService.ts
+++ src/api/file/FileService.ts
@@ -5,25 +5,29 @@ import { Injectable } from '@nestjs/common';
 export class FileService {
   private readonly fs = require('fs');
   constructor() { }
-  deleteFile(pathFile:string) {
+  deleteFile(pathFile: string) {
     if (this.checkFile(pathFile)) {
       this.fs.unlinkSync(pathFile);
     }
   }
-  checkFile(pathFile:string) {
+  checkFile(pathFile: string) {
     return this.fs.existsSync(pathFile);
   }
 
-  getFile(pathFile:string) {
+  getFile(pathFile: string) {
     if (this.checkFile(pathFile)) {
       return this.fs.readFileSync(pathFile);
     }
   }
+  /**Возможно нужно проверка что каталог существует */
+  getCatalog(pathFile: string) {
+    return this.fs.readdirSync(pathFile);
+  }
 
-  setFile(pathFile:string, fileName:string, data: string) {
+  setFile(pathFile: string, fileName: string, data: string) {
     if (pathFile) {
       !this.fs.existsSync(`${pathFile}`) && this.fs.mkdirSync(`${pathFile}`, { recursive: true })
-    }    
+    }
     this.fs.open(`${pathFile}\\${fileName}`, 'w', (err: Error) => {
       if (err) throw err;
     });
